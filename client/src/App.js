74a4e9b704c2c9041d5dff79f29a038a581f9399
Weslley Braga <wesbragagt@gmail.com>
2019-07-14 23:16:47 -0500

    loggin to console salaries based on calculation of time

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,81 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
+
+import SalaryForm from "./Components/SalaryForm";
+
 import "./App.css";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    state = {
+        seconds: 0,
+        minutes: 0,
+        hours: 0,
+        salaryInput: "",
+        salary: null
+    };
+
+    componentDidUpdate() {
+        console.log(this.state);
+    }
+
+    addOne = currentValue => {
+        this.setState({
+            timer: (currentValue += 1)
+        });
+    };
+
+    startTimer = currentValue => {
+        setInterval(() => {
+            this.setState({
+                seconds: (currentValue += 1)
+            });
+
+            if (this.state.seconds > 60) {
+                this.setState({
+                    seconds: 0
+                });
+            }
+        }, 1000);
+    };
+
+    calcSalaryTime = value => {
+        const moneyPerTime = {
+            hour: value / 52 / 40,
+            min: value / 52 / 40 / 60,
+            second: value / 52 / 40 / 60 / 60
+        };
+
+        console.log(moneyPerTime);
+    };
+
+    handleChange = value => {
+        this.setState({ salaryInput: value });
+    };
+
+    handleSubmit = event => {
+        event.preventDefault();
+        const { salary } = event.target;
+        console.log(salary.value);
+        this.setState(
+            {
+                salary: salary.value
+            },
+
+            () => {
+                this.calcSalaryTime(this.state.salary);
+            }
+        );
+    };
+    render() {
+        return (
+            <div className="App">
+                <SalaryForm
+                    inputVal={this.state.salaryInput}
+                    handleChange={e => this.handleChange(e.target.value)}
+                    handleForm={e => this.handleSubmit(e)}
+                />
+            </div>
+        );
+    }
 }
 
 export default App;
---
+++ client/src/Components/SalaryForm.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function SalaryForm({ inputVal, handleChange, handleForm }) {
+    return (
+        <div className="salary_form">
+            <form onSubmit={handleForm}>
+                <label>Salary/Hour</label>
+                <input
+                    name="salary"
+                    placeholder="$50,000"
+                    value={inputVal}
+                    onChange={handleChange}
+                />
+                <button type="submit">Submit</button>
+            </form>
+        </div>
+    );
+}
